5c1500e798f42abb35e097c7cf4c82587c38c566
harrymkwn <heratmakwana19@gmail.com>
2024-04-26 04:46:12 +0530

    Basice server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
+import express  from "express";
+import dotenv from 'dotenv'
 
+const port = 3000;
 
-console.log("Hello world");
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', client => {
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to hhld");
+});
+
+server.listen(port, ()=>{
+    console.log("Your app is running on 3000");
+})
